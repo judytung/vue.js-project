35c408ea82d44794cc734a44db584ff864d522c7
TUNG HSIAO HAN <sky250331@gmail.com>
2022-02-09 20:51:05 +0800

    新增產品元件跟產品刪除元件

--- js/products.js
+++ js/products.js
@@ -21,7 +21,7 @@ const app = createApp({
             tempProducts:{
                 imagesUrl:[]   // 新增多圖使用，使用陣列
             },
-            new: false,
+            isNew: false,
             pagination: {}  // 先定義一個分頁物件
         }
     },
@@ -49,17 +49,6 @@ const app = createApp({
             // })
 
         },
-        removeProduct () {
-            axios.delete(`${apiUrl}/api/${path}/admin/product/${this.tempProducts.id}`)
-            .then((res) => {
-            removeProductModal.hide();
-            this.getProducts();
-            })
-            .catch((err) => {
-            console.dir(err);
-            alert(err.data.message)
-            })
-        },
         checkLogin(params) {
             axios.post(`${apiUrl}/api/user/check`)
             .then(() => {
@@ -78,34 +67,18 @@ const app = createApp({
                     imagesUrl:[]   // 若為新增產品，將欄位清空
                 }
                 productModal.show(); 
-                this.new = true;
+                this.isNew = true;
             } else if (status === 'edit') {
                 this.tempProducts = {...productItem}; // 因物件傳參考的特性，需做拷貝
                 productModal.show(); 
-                this.new = false;
+                this.isNew = false;
             } else if (status === 'remove') {
                 removeProductModal.show();
                 this.tempProducts = {...productItem}; // 因物件傳參考的特性，需做拷貝
             }
             // console.log(status, productItem);
         },
-        // 新增編輯都能使用的話，改成  updateProduct
-        updateProduct() {
-            let http = 'post';
-            let url = `${apiUrl}/api/${path}/admin/product`;
-            // 判斷是不是新的產品，當 this.new 為 false 時會跑這段
-            if (!this.new) {
-                http = 'put';
-                url = `${apiUrl}/api/${path}/admin/product/${this.tempProducts.id}`;
-            }
-            axios[http](url, {data:this.tempProducts}) // 這邊要特別注意，因為 api 格式規定資料是放在 data 裡的，所以需將 temproduct 放在 data 裡傳送
-            .then((res) => {
-                alert(res.data.message)
-                // console.log(res.data);
-                this.getProducts();  // 需重新發送取得產品請求，本地端才會更新
-                productModal.hide(); 
-            })
-        }
+        
     },
    mounted () {
     this.checkLogin();
@@ -114,4 +87,54 @@ const app = createApp({
     removeProductModal = new bootstrap.Modal(document.getElementById('removeProductModal'));
    }
 });
-app.mount('#app')
+
+// 全域註冊
+// 產品元件
+app.component('productModal', {
+    // 需接受外層資料來運作，使用 props 
+    props: ['tempProducts','isNew'],
+    template: '#templateForProductModal', 
+    // 內層少了 updateProduct 的方法，所以將方法移進來
+    // 因為更新產品是在 modal 裏面做的，而我們將這個 modal 做成元件，所以更新的方法要放在元件裡面，每個元件的資料都是獨立的，所以進入元件時，方法也都是獨立的
+    methods: {
+        // 新增編輯都能使用的話，改成  updateProduct
+        updateProduct() {
+            let http = 'post';
+            let url = `${apiUrl}/api/${path}/admin/product`;
+            // 判斷是不是新的產品，當 this.new 為 false 時會跑這段
+            if (!this.isNew) {
+                http = 'put';
+                url = `${apiUrl}/api/${path}/admin/product/${this.tempProducts.id}`;
+            }
+            axios[http](url, {data:this.tempProducts}) // 這邊要特別注意，因為 api 格式規定資料是放在 data 裡的，所以需將 temproduct 放在 data 裡傳送
+            .then((res) => {
+                alert(res.data.message)
+                // console.log(res.data);
+                // this.getProducts();  // 需重新發送取得產品請求，本地端才會更新 // 將 updateProduct 移進內層後就沒有 getProducts 
+                // 所以這裡無法使用外層方法 getProducts ，要使用 emit 來觸發外層事件
+                this.$emit('get-products');
+                productModal.hide(); 
+            })
+        }
+    }
+});
+// 產品刪除元件
+app.component('removeProductModal', {
+    props: ['tempProducts'],
+    template: '#templateForRemoveModal',
+    methods: {
+        removeProduct () {
+            axios.delete(`${apiUrl}/api/${path}/admin/product/${this.tempProducts.id}`)
+            .then((res) => {
+            removeProductModal.hide();
+            // this.getProducts();
+            this.$emit('get-products');
+            })
+            .catch((err) => {
+            console.dir(err);
+            alert(err.data.message)
+            })
+        },
+    }
+})
+app.mount('#app');
